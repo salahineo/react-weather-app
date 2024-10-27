32c784c584f133c3761a43cc7d4526987721766a
Mohamed Salah <salahineo.personal@gmail.com>
2024-10-27 22:45:48 +0300

    refactoring

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,27 +8,7 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-container">
         <div className="copyright">
-          © <span>{String(footer_copyright_year.getFullYear())}</span> | <span className="author">Mohamed Salah</span>
-        </div>
-        <div className="links">
-          <a href="https://github.com/salahineo" target="_blank" rel="noreferrer">
-            <i className="fab fa-github" />
-          </a>
-          <a href="https://www.linkedin.com/in/salahineo/" target="_blank" rel="noreferrer">
-            <i className="fab fa-linkedin-in" />
-          </a>
-          <a href="https://twitter.com/salahineo" target="_blank" rel="noreferrer">
-            <i className="fab fa-twitter" />
-          </a>
-          <a href="https://www.facebook.com/salahineo/" target="_blank" rel="noreferrer">
-            <i className="fab fa-facebook-f" />
-          </a>
-          <a href="https://salahineo.github.io/salahineo/" target="_blank" rel="noreferrer">
-            <i className="fas fa-globe-africa" />
-          </a>
-          <a href="mailto:salahineo.work@gmail.com" target="_blank" rel="noreferrer">
-            <i className="fas fa-envelope" />
-          </a>
+          © <span>{String(footer_copyright_year.getFullYear())}</span> | <a href='https://salahineo.com' target='_blank' rel="noopener noreferrer" className="author">Mohamed Salah</a>
         </div>
       </div>
     </footer>
